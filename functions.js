6a111aa2e07eac2a48404996ddbe0b76b7ae5f96
KommuriVaishnavi <kommurivaishnavi29@gmail.com>
2025-05-27 16:24:39 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,17 @@
+// functions
+//  1)using function keyword
+function hello(){
+    // console.log('hello from function');
+    return 2+2;
+}
+console.log(hello());
+// 2) variable expression
+const some= function hello(){
+    return 2+2;
+}
+console.log(some());
+// 3) anonymous expression
+const x=function(){
+    return 2+2;
+}
+console.log(x());
